d42966b84a0ac4ca10a894b51078f50ae53d60f9
Asha Pappusamy <ashabaeng23@gmail.com>
2025-07-13 14:45:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import sqlalchemy as sqlalchemy
+engine = sqlalchemy.create_engine("mysql+mysqlconnector://3qKZvyc8Bw7Ckf1.root:ixPIapSBo4owm2Qf@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/Asteroids")
+import sqlalchemy
 import streamlit as st
 st.write("This is my Heading")
 st.write("hello")
