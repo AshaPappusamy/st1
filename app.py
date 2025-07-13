395216de026fdab4a57ceb64bfd24398656f4f85
Asha Pappusamy <ashabaeng23@gmail.com>
2025-07-13 14:41:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 import streamlit as st
 st.write("This is my Heading")
 st.write("hello")
+st.set_page_config(page_title="NASA Asteroid Dashboard", layout="wide")
+st.title("🚀 NASA Near-Earth Object (NEO) Insights")
