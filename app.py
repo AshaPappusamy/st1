b0c2a187b6c883c3ac7388e9cd251083b9c24fdd
Asha Pappusamy <ashabaeng23@gmail.com>
2025-07-13 14:53:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import sqlalchemy as sqlalchemy_package
+from sqlalchemy import create_engine, text
 engine = sqlalchemy_package.create_engine("mysql+mysqlconnector://3qKZvyc8Bw7Ckf1.root:ixPIapSBo4owm2Qf@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/Asteroids")
 import streamlit as st
+import pandas as pd
 st.write("This is my Heading")
 st.write("hello")
 st.set_page_config(page_title="NASA Asteroid Dashboard", layout="wide")
 st.title("🚀 NASA Near-Earth Object (NEO) Insights")
-import pandas as pd
-from sqlalchemy import create_engine, text
+
+
